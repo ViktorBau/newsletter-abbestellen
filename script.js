c9cf5efd7b68c2eefc866f732126345ad5b02bb5
ViktorBau <116071276+ViktorBau@users.noreply.github.com>
2024-07-21 18:24:51 +0200

    test1

---
+++ script.js
@@ -0,0 +1,23 @@
+// Liste der Bilder
+const images = [
+    'bilder/1.jpeg',
+     'bilder/2.jpeg',
+    'bilder/3.jpeg',
+    // fügen Sie hier Ihre weiteren Bilder hinzu
+];
+
+// Zufälliges Bild auswählen
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    return images[randomIndex];
+}
+
+// Bild setzen
+function setRandomImage() {
+    const randomImage = getRandomImage();
+    const imgElement = document.getElementById('randomImage');
+    imgElement.src = randomImage;
+}
+
+// Wenn das Dokument geladen ist, setze das zufällige Bild
+document.addEventListener('DOMContentLoaded', setRandomImage);
